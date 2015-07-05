c8f546b0ca9eb3a9f31ebec720d2993b5f916d42
TheByteGuru <youtube@thebyteguru.com>
2015-07-05 16:29:17 +0300

    Initial commit

---
+++ Tanki/src/com/thebyteguru/main/Main.java
@@ -0,0 +1,13 @@
+package com.thebyteguru.main;
+
+import com.thebyteguru.display.Display;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Display.create(800, 600, "Tanks");
+
+	}
+
+}
