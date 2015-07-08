271ef2cffa8d1d1afc7f1b02272c6ee9b46d31cf
letifer85 <letifer85@gmail.com>
2015-07-08 18:52:06 +0300

    Пишем игру на Java(Ява) - Клон Танков - Улучшаем графику - [4]

--- Tanks/src/com/thebyteguru/display/Display.java
+++ Tanks/src/com/thebyteguru/display/Display.java
@@ -4,6 +4,9 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 import javax.swing.JFrame;
 
@@ -13,25 +16,27 @@ public abstract class Display {
 	private static JFrame window;
 	private static Canvas content;
 
-	public static void create(int width, int height, String title) {
+	private static BufferedImage buffer;
+	private static int[] bufferData;
+	private static Graphics bufferGraphics;
+	private static int clearColor;
+
+	// temp
+	private static float delta = 0;
+
+	// temp end
+
+	public static void create(int width, int height, String title, int _clearColor) {
 
 		if (created)
 			return;
 
 		window = new JFrame(title);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		content = new Canvas() {
-
-			public void paint(Graphics g) {
-				super.paint(g);
-				render(g);
-			}
-
-		};
+		content = new Canvas();
 
 		Dimension size = new Dimension(width, height);
 		content.setPreferredSize(size);
-		content.setBackground(Color.black);
 
 		window.setResizable(false);
 		window.getContentPane().add(content);
@@ -39,17 +44,28 @@ public abstract class Display {
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 
+		buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
+		bufferGraphics = buffer.getGraphics();
+		clearColor = _clearColor;
+
+		created = true;
+
+	}
+
+	public static void clear() {
+		Arrays.fill(bufferData, clearColor);
 	}
 
 	public static void render() {
-		content.repaint();
+		bufferGraphics.setColor(new Color(0xff0000ff));
+		bufferGraphics.fillOval((int) (350 + (Math.sin(delta) * 200)), 250, 100, 100);
+		delta += 0.02f;
 	}
 
-	private static void render(Graphics g) {
-
-		g.setColor(Color.white);
-		g.fillOval(400 - 50, 300 - 50, 100, 100);
-
+	public static void swapBuffers() {
+		Graphics g = content.getGraphics();
+		g.drawImage(buffer, 0, 0, null);
 	}
 
 }
--- Tanks/src/com/thebyteguru/main/Main.java
+++ Tanks/src/com/thebyteguru/main/Main.java
@@ -11,12 +11,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Display.create(800, 600, "Tanks");
+		Display.create(800, 600, "Tanks", 0xff00ff00);
 
 		Timer t = new Timer(1000 / 60, new AbstractAction() {
 
 			public void actionPerformed(ActionEvent e) {
+				Display.clear();
 				Display.render();
+				Display.swapBuffers();
 			}
 
 		});
